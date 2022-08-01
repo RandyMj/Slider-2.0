90b3d4c3d84762812071c2bd9adeb1b4c0b9a63b
RandyM <randymotaj@gmail.com>
2022-07-31 20:10:42 -0400

    Adding selectors module

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,32 @@
 const prev = document.getElementById('btnPrev');
 const next = document.getElementById('btnNext');
 const imgContainer = document.getElementById('img-container');
+const bar = document.querySelectorAll(".point");
 
-const numberOfElements = 5
+const numberOfElements = bar.length
 const divSpace = 100;
 const imgSpace = divSpace / numberOfElements;
 
 let i = 0;
 
+// NEW CODE FOR THE SELECTION BARS
+bar.forEach( (bars, i) => {
+    bar[i].addEventListener('click', () => {
+        let position = i;
+        let positionCalc = position * -`${imgSpace}`;
+
+        imgContainer.style.transform = `translateX(${positionCalc}%)`;
+
+        bar.forEach (( bars, i) => {
+            bar[i].classList.remove('active');
+        });
+
+        bar[i].classList.add('active');
+    })
+});
+
+
+// CODE WRITEN FOR THE PREV AND NEXT BUTTONS---------------------------------------------------------------------
 function positionNext () {
     if ( i <= 0 && i > - `${divSpace - imgSpace}`) {
         i = i - `${imgSpace}`;
@@ -20,18 +39,14 @@ function positionNext () {
 function positionPrev () {
     if ( i == 0) {
         i = i - `${divSpace - imgSpace}`;
-        console.log(i);
     } 
     else if ( i < 0 ) {
         // Dont know why it takes the two variables and concatenates them. Left the code commented for later review.
-        // i = i + `${imgSpace}`;
-        console.log(`${imgSpace}`);
+        // i = i+`${imgSpace}`;
         i = i + 20;
-        console.log(i);
     }
     imgContainer.style.transform = `translateX(${i}%)`
 }
-
 
 prev.addEventListener('click', positionPrev);
 next.addEventListener('click', positionNext);
