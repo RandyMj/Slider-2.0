b41a9e16969ce01cf58a4edfc6c60e646f15450c
RandyM <randymotaj@gmail.com>
2022-08-04 11:12:07 -0400

    Adding full scalability

--- src/js/index.js
+++ src/js/index.js
@@ -22,13 +22,13 @@ bar.forEach( (bars, i) => {
         });
 
         bar[i].classList.add('active');
+        console.log(i)
     })
 });
 
-
 // CODE WRITEN FOR THE PREV AND NEXT BUTTONS---------------------------------------------------------------------
 function positionNext () {
-    if ( i <= 0 && i > - `${divSpace - imgSpace}`) {
+    if ( i <= 0 && i > -`${divSpace - imgSpace}`) {
         i = i - `${imgSpace}`;
     } else if( i == - `${divSpace - imgSpace}`) {
         i = 0;
@@ -37,13 +37,12 @@ function positionNext () {
 }
 
 function positionPrev () {
-    if ( i == 0) {
+    if ( i == 0 ) {
         i = i - `${divSpace - imgSpace}`;
     } 
-    else if ( i < 0 ) {
-        // Dont know why it takes the two variables and concatenates them. Left the code commented for later review.
-        // i = i+`${imgSpace}`;
-        i = i + 20;
+    else if ( i >= -`${divSpace - imgSpace}`) {
+        // Dont know why it takes the two variables and concatenates them. Left the code commented for later review. I use to plus simbols because in this instance the i is negative and we need to asign a positive simbol to the variable for the interpreter take it as a positive number and make the operation.
+        i = i + +`${imgSpace}`;
     }
     imgContainer.style.transform = `translateX(${i}%)`
 }
